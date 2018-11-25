5fecce28247a069debbbf0901ea53579fe75160a
kisekitw <kisekitw@gmail.com>
2018-11-25 08:59:07 +0800

    Emit newMessage oncreateMessage

--- server/server.js
+++ server/server.js
@@ -16,14 +16,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'opopop',
-        text: 'aaaaa',
-        createAt: '111222333'
-    });
-
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {

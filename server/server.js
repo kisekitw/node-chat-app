d943543ba064be643e771386fc35e91d19bb2796
kisekitw <kisekitw@gmail.com>
2018-11-25 09:11:41 +0800

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -16,6 +16,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the cat app'
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
 
@@ -24,6 +35,12 @@ io.on('connection', (socket) => {
             text: message.text,
             createAt: new Date().getTime()
         });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
